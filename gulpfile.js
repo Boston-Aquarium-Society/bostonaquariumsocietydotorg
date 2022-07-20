1d664061446d8ddd0be683c3872e5b1d19b2d2a4
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2022-07-20 11:55:33 +0800

    Rename /packages to /themes

--- gulpfile.js
+++ gulpfile.js
@@ -51,8 +51,6 @@ function css(done) {
 function js(done) {
     pump([
         src([
-            'node_modules/@tryghost/shared-theme-assets/assets/js/v1/lib/**/*.js',
-            'node_modules/@tryghost/shared-theme-assets/assets/js/v1/main.js',
             'assets/js/lib/*.js',
             'assets/js/main.js'
         ], {sourcemaps: true}),
